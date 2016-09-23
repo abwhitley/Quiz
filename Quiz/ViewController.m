3c911c1145be559eb18faad72658e01f4c84450a
abwhitley <a.blakewhitley@gmail.com>
2016-09-23 15:27:05 -0400

    Finished chapter 9

--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -9,21 +9,74 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic) IBOutlet UILabel *currentQuestionLabel;
+@property (nonatomic) IBOutlet UILabel *nextQuestionLabel;
+@property (nonatomic) IBOutlet UILabel *answerLabel;
+@property (nonatomic) IBOutlet NSLayoutConstraint *currentQuestionLabelCenterXConstraint;
+@property (nonatomic) IBOutlet NSLayoutConstraint *nextQuestionLabelCenterXConstraint;
+@property (nonatomic) NSArray *questions;
+@property (nonatomic) NSArray *answers;
+@property (nonatomic) int currentQuestionIndex;
 @end
 
 @implementation ViewController
+- (void)animateLabelTransitions {
+    [self.view layoutIfNeeded];
+    CGFloat screenWidth = self.view.frame.size.width;
+    self.nextQuestionLabelCenterXConstraint.constant = 0;
+    self.currentQuestionLabelCenterXConstraint.constant += screenWidth;
+    [UIView animateWithDuration:0.5
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveLinear
+                     animations:^{
+                         self.currentQuestionLabel.alpha = 0.0; self.nextQuestionLabel.alpha = 1.0; [self.view layoutIfNeeded];
+                     }
+                     completion:^(BOOL finished) {
+                         UILabel *tempLabel = self.currentQuestionLabel;
+                         self.currentQuestionLabel = self.nextQuestionLabel;
+                         self.nextQuestionLabel = tempLabel;
+                         NSLayoutConstraint *tempConstraint =
+                         self.currentQuestionLabelCenterXConstraint;
+                         self.currentQuestionLabelCenterXConstraint = self.
+                         nextQuestionLabelCenterXConstraint;
+                         self.nextQuestionLabelCenterXConstraint = tempConstraint;
+                         [self updateOffScreenLabel];
+                     }];
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.nextQuestionLabel.alpha = 0.0;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.questions = @[ @"What is your name?",
+                        @"What is your quest?",
+                        @"What is your favorite color?" ];
+    self.answers = @[ @"Sir Galahad of Camelot",
+                      @"I seek the Grail",
+                      @"Blue. No, yel-" ];
+    self.currentQuestionLabel.text = self.questions[self.currentQuestionIndex];
+    [self updateOffScreenLabel];
+}
+- (void)updateOffScreenLabel {
+    CGFloat screenWidth = self.view.frame.size.width;
+    self.nextQuestionLabelCenterXConstraint.constant = -screenWidth;
+}
+- (IBAction)showNextQuestion:(id)sender {
+    self.currentQuestionIndex++;
+    if (self.currentQuestionIndex == self.questions.count) {
+        self.currentQuestionIndex = 0;
+    }
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.nextQuestionLabel.text = question;
+    self.answerLabel.text = @"???";
+    [self animateLabelTransitions];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(IBAction)showAnswer:(id)sender{
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
 }
-
 
 @end
